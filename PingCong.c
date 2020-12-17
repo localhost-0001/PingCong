27c81d5f603d8bc64c9f9286c2316715b836ea3a
localhost <localhost>
2020-12-17 14:02:20 +0100

    Add static rackets, fix max_{x,y}

--- PingCong.c
+++ PingCong.c
@@ -17,6 +17,8 @@ void invertDeltaX(Ball);
 int main(){
 	//declare some stuff
 	int m_x, m_y; //max_{x,y}
+	struct Ball GameBall; 
+	struct Racket leftRacket, rightRacket;
 
 	printf("Hello World!");
 	initscr(); //Set-up all screen stuffs
@@ -26,9 +28,9 @@ int main(){
 	border('X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'); //Add border around screen
 
 	getmaxyx(stdscr, m_y, m_x); // How much is the screen?
-
+	m_x--; // It's 1 too many, if not doing this text falls of the screen
+	m_y--;
 	//Now let's create the Ball
-	struct Ball GameBall; 
 	GameBall.deltaX = 10;
 	GameBall.deltaY = 0;
 	GameBall.cor_x = m_x/2; //Set to somewhat of the middle of the screen
@@ -36,6 +38,22 @@ int main(){
 	mvprintw(GameBall.cor_y, GameBall.cor_x, "O"); //Now show the the Ball at the initial Position
 
 	//Now set-up the rackets
+	leftRacket.size = m_y/10; //set right size, for both of them
+	rightRacket.size = leftRacket.size;
+	leftRacket.pos_y = m_y/2; //somewhat middle of y
+	rightRacket.pos_y = leftRacket.pos_y;
+	leftRacket.pos_x = 0;
+	rightRacket.pos_x = m_x;
+
+	mvprintw(m_y/2, m_x/2, "%d", leftRacket.size);
+	mvprintw(m_y/2+1, m_x/2, "%d", leftRacket.pos_y);
+	mvprintw(m_y/2-1, m_x/2, "%d", leftRacket.pos_x);
+
+	for(int i = 0; i < leftRacket.size; i++){
+		mvprintw(leftRacket.pos_y-i, leftRacket.pos_x, "|");
+		mvprintw(rightRacket.pos_y-i, rightRacket.pos_x-1, "|");
+		continue;
+	}
 
 	refresh(); // Show first screen
 	sleep(10000);
